6f5ecbbad08f3b75c264a2f2d4f25d77cc67f828
JumpNShootMan <danielnr96@gmail.com>
2019-07-16 22:25:45 -0500

    Push fixes

--- ListRedditPosts.go
+++ ListRedditPosts.go
@@ -2,6 +2,7 @@ package main
 
 import "github.com/turnage/graw/reddit"
 
+//ListNRedditPosts(n int, subreddit string)
 func ListNRedditPosts(n int, subreddit string) (reddit.Bot, error) {
 
 	bot, err := reddit.NewBotFromAgentFile("agent", 0)
