0b40e53fcc7fc97745be512a607abe8c41295a65
JumpNShootMan <danielnr96@gmail.com>
2019-07-16 22:29:56 -0500

    fix gitignore

--- ListRedditPosts.go
+++ ListRedditPosts.go
@@ -3,12 +3,15 @@ package main
 import "github.com/turnage/graw/reddit"
 
 //ListNRedditPosts(n int, subreddit string)
+
 func ListNRedditPosts(n int, subreddit string) (reddit.Bot, error) {
 
 	bot, err := reddit.NewBotFromAgentFile("agent", 0)
 
 	if err != nil {
+		println("Error en creacion de bot")
 		return nil, err
-		println("error en creacion de bot")
 	}
+
+	return bot, err
 }
